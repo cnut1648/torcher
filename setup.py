dd17bf13416ff316bcf0b382392c637fd385a25b
Jiashu XU <1999J0615une@gmail.com>
2021-01-14 14:02:37 -0800

    change disutils to setuptools

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 
-# from setuptools import find_packages, setup
-from distutils.core import setup
-
-# with open('README.md') as readme_file:
-#     readme = readme_file.read()
-#
-# with open('requirements.txt') as requirements_file:
-#     requirements = requirements_file.readlines()
+# from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='Torcherist',
-    version='0.1.1',
+    version='0.1.2',
     description='utils codes for pyTorch',
     author="Jiashu Xu",
     author_email='1999J0615une@gmail.com',
     url='https://github.com/cnut1648/torcher',
-    download_url='https://github.com/cnut1648/torcher/archive/v0.1.tar.gz',
-    packages=["torcherist"],
+    download_url='https://github.com/cnut1648/torcherist/archive/v0.1.2.tar.gz',
+    # packages=["torcherist"],
+    packages = find_packages(),
     license="MIT license",
     keywords=['nlp', 'skills', 'onet', 'pytorch'],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'beautifulsoup4',
         'numpy',
         'graphviz',
